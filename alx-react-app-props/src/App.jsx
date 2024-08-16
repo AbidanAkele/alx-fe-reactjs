a6ae5edb4998acb4370c5bdcc36fc436786aa695
Abidan Akele <abidansirah@gmail>
2024-08-16 16:02:11 +0200

    week 10 task 2

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import ProfilePage from './components/ProfilePage';
-import { UserContext } from './components/UserContext';
+import UserProfile from './UserProfile';
+import { UserContext } from './UserContext';
 
 function App() {
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+  const userData = { name: "Alice", age: 25, bio: "Loves hiking and photography" };
 
   return (
     <UserContext.Provider value={userData}>
-      <ProfilePage />
+      <UserProfile />
     </UserContext.Provider>
   );
 }
--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,11 +1,16 @@
-function UserProfile(props) {
-    return (
-        <div>
-            <h2>{props.name}</h2>
-            <p>Age: {props.age}</p>
-            <p>Bio: {props.bio}</p>
-        </div>
-    );
-}
+import React, { useContext } from 'react';
+import { UserContext } from './UserContext';
+
+const UserProfile = () => {
+  const { name, age, bio } = useContext(UserContext);
+
+  return (
+    <div>
+      <h2>{name}</h2>
+      <p>Age: {age}</p>
+      <p>Bio: {bio}</p>
+    </div>
+  );
+};
 
 export default UserProfile;
