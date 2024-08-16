b3a3f646dd4706b46fb6d55d7e2cab247257016d
Abidan Akele <abidansirah@gmail>
2024-08-16 15:27:06 +0200

    week 10 task 0

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: 'navy', color: 'white', padding: '10px', textAlign: 'center', width: '100%'}}>
+      <p style={{ margin: '0' }}>© 2023 City Lovers</p>
+    </footer>
+  );
+}
+
+export default Footer;
