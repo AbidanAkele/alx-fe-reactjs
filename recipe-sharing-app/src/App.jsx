8bc76f2acd7aede5d254349b865ae143e741fe7c
Abidan Akele <abidansirah@gmail>
2024-08-24 11:08:58 +0200

    week 11 task 1

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,13 +1,27 @@
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
+import RecipeDetails from './components/RecipeDetails';
 
 function App() {
   return (
-    <div>
-      <h1>Recipe Sharing Application</h1>
-      <AddRecipeForm />
-      <RecipeList />
-    </div>
+    <Router>
+      <div>
+        <h1>Recipe Sharing Application</h1>
+        <nav>
+          <Link to="/">Home</Link>
+        </nav>
+        <Switch>
+          <Route exact path="/">
+            <AddRecipeForm />
+            <RecipeList />
+          </Route>
+          <Route path="/recipe/:id" render={({ match }) => (
+            <RecipeDetails recipeId={Number(match.params.id)} />
+          )} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
